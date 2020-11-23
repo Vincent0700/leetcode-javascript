13bb66dd615915f57f63e5733b3447f4bde765e8
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-11-24 07:54:24 +0800

    feat: add s0222

---
+++ daily/222.完全二叉树的节点个数.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=javascript
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+  if (!root) return 0;
+  const left = countNodes(root.left);
+  const right = countNodes(root.right);
+  return left + right + 1;
+};
+// @lc code=end
