8e0e5c6d631fd266e2598272ab7ae4ff49b2d181
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-11-25 00:24:10 +0800

    feat: add s0344

---
+++ daily/344.反转字符串.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=javascript
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  s.reverse();
+};
+// @lc code=end
