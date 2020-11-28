fd1d6d6a2b3ce3fb97f8bcc4b85056bc82de4730
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-11-28 20:01:11 +0800

    feat: add s0504

---
+++ daily/504.七进制数.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=javascript
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+  return num.toString(7);
+};
+// @lc code=end
