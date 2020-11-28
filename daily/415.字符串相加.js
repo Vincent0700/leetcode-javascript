8312aa4298f13b45e699b6410ac956e3d678ac6f
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-11-28 19:32:56 +0800

    feat: add s0415

---
+++ daily/415.字符串相加.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=415 lang=javascript
+ *
+ * [415] 字符串相加
+ */
+
+// @lc code=start
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+  return (BigInt(num1) + BigInt(num2)).toString();
+};
+// @lc code=end
