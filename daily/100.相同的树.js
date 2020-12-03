9d21eca90addfe8be1ca1c3c1c55af4b0b1bc23c
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-12-03 16:48:58 +0800

    feat: s0100

---
+++ daily/100.相同的树.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=javascript
+ *
+ * [100] 相同的树
+ */
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+// @lc code=start
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (!p && !q) return true;
+  if ((!p && q) || (p && !q) || p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+// @lc code=end
