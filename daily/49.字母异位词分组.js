bc92c044eb2d974d85ab1910fbaab67694fb8582
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-12-14 09:58:26 +0800

    feat: s0049

---
+++ daily/49.字母异位词分组.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=49 lang=javascript
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  const obj = [];
+  const arr = strs.map((w) => [
+    w,
+    w
+      .split('')
+      .sort()
+      .join('')
+  ]);
+  arr.forEach((a) => {
+    if (a[1] in obj) obj[a[1]].push(a[0]);
+    else obj[a[1]] = [a[0]];
+  });
+  return Object.values(obj);
+};
+// @lc code=end
+
+const strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
+console.log(groupAnagrams(strs));
