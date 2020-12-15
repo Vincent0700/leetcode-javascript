1523162e2853ffc1494856ef739fc3811be35082
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-12-15 09:55:34 +0800

    feat: s1431

---
+++ daily/1431.拥有最多糖果的孩子.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1431 lang=javascript
+ *
+ * [1431] 拥有最多糖果的孩子
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+  const max = Math.max(...candies);
+  return candies.map((n) => n + extraCandies >= max);
+};
+// @lc code=end
