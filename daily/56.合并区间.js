3015f8d892b49fcb94d2e2262e71ee72b52319a7
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-12-15 18:08:06 +0800

    feat: s0056

---
+++ daily/56.合并区间.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=javascript
+ *
+ * [56] 合并区间
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+  intervals.sort((a, b) => (a[1] !== b[1] ? a[1] - b[1] : a[0] - b[0]));
+  let i = 1;
+  while (i < intervals.length) {
+    if (intervals[i - 1][0] >= intervals[i][0]) {
+      intervals[i - 1] = null;
+      intervals.splice(--i, 1);
+    } else if (intervals[i - 1][1] >= intervals[i][0]) {
+      intervals[i] = [intervals[i - 1][0], intervals[i][1]];
+      intervals[i - 1] = null;
+      intervals.splice(--i, 1);
+    } else i++;
+    if (i === 0) i++;
+  }
+  return intervals.filter((t) => t);
+};
+// @lc code=end
+
+const intervals = [
+  [1, 4],
+  [0, 2],
+  [3, 5]
+];
+console.log(merge(intervals));
