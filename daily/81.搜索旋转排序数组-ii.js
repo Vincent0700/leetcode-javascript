1bc4c77a7b1cda17f73226723ad0c0c388d4bcaa
Vincent0700 <wang.yuanqiu007@gmail.com>
2020-12-16 13:42:00 +0800

    feat: s0081

---
+++ daily/81.搜索旋转排序数组-ii.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=81 lang=javascript
+ *
+ * [81] 搜索旋转排序数组 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+  return nums.includes(target);
+};
+// @lc code=end
