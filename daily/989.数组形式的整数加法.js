da5c1c4f1c62371017fe0b37570bf33dee71cbc6
Vincent0700 <wang.yuanqiu007@gmail.com>
2021-01-22 10:20:06 +0800

    feat: s0989

---
+++ daily/989.数组形式的整数加法.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=989 lang=javascript
+ *
+ * [989] 数组形式的整数加法
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number[]}
+ */
+var addToArrayForm = function(A, K) {
+  return (BigInt(A.join('')) + BigInt(K)).toString().split('');
+};
+// @lc code=end
