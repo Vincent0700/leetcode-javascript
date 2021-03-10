9f3f248926fd754bd150889ed2fa75bd2b598d7f
Vincent0700 <wang.yuanqiu007@gmail.com>
2021-03-10 10:21:51 +0800

    feat: s0224

---
+++ daily/224.基本计算器.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=224 lang=javascript
+ *
+ * [224] 基本计算器
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+  return eval(s);
+};
+// @lc code=end
