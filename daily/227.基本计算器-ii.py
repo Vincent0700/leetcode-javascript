f65ea172cfe97eb637495c9618bacc3d53576687
Vincent0700 <wang.yuanqiu007@gmail.com>
2021-03-11 09:49:16 +0800

    feat: s0227

---
+++ daily/227.基本计算器-ii.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=227 lang=python3
+#
+# [227] 基本计算器 II
+#
+
+# @lc code=start
+class Solution:
+    def calculate(self, s: str) -> int:
+        return eval(s.replace('/', '//'))
+# @lc code=end
+
