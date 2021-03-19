468efe509c6fa321eefd69c9058a9df5017c022a
Vincent0700 <wang.yuanqiu007@gmail.com>
2021-03-19 10:15:11 +0800

    feat: s1603

---
+++ daily/1603.设计停车系统.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1603 lang=javascript
+ *
+ * [1603] 设计停车系统
+ */
+
+// @lc code=start
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+  this.counts = [big, medium, small];
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+  return this.counts[carType - 1]-- > 0;
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
+// @lc code=end
