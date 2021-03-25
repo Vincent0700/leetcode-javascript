dc8f10b5e285085cb3b44b6a2d177f186df0aa23
Vincent0700 <wang.yuanqiu007@gmail.com>
2021-03-25 10:11:51 +0800

    feat: s0535

---
+++ daily/535.tiny-url-的加密与解密.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=535 lang=javascript
+ *
+ * [535] TinyURL 的加密与解密
+ */
+
+// @lc code=start
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+  return longUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+  return shortUrl;
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
+// @lc code=end
